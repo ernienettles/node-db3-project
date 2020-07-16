0752212b9b83e04e807f7d36e9ec2eb38cb3e2b9
Ernie Nettles <ernienettles@gmail.com>
2020-07-16 17:29:39 -0400

    First commit

--- queries.sql
+++ queries.sql
@@ -1,9 +1,27 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+    select product.ProductName, category.CategoryName
+    from product
+    join category on product.CategoryId = category.Id
+    order by CategoryName
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+    select [order].Id, shipper.CompanyName
+    from [order]
+    join shipper on [order].ShipVia = shipper.Id 
+    where [order].OrderDate < date('2012-08-09') 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+    select orderdetail.Quantity, product.ProductName
+    from orderdetail
+    join product on product.id = orderdetail.ProductId
+    where orderdetail.orderid = 10251
+    order by product.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+    select [order].Id as OrderId, customer.CompanyName, employee.LastName
+    from [order]
+    join customer on customer.id = [order].CustomerId
+    join employee on employee.id = [order].EmployeeId
+
---
+++ schemes/scheme-model.js
@@ -0,0 +1,56 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+function findSteps(id) {
+    return db('steps')
+        .where({ scheme_id: id })
+        .join('schemes', 'schemes.id', '=', 'steps.scheme_id')
+        .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .orderBy('steps.step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(ids => {
+            return findById(ids[0])
+        })
+}
+
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+        .then(() => {
+            return findById(id)
+        })
+}
+
+async function remove(id) {
+    const removed = await findById(id);
+    return db('schemes')
+        .where({ id })
+        .del()
+        .then(() => {
+            return removed;
+        })
+}
